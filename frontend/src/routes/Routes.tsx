844258b0fe73d9dec75a89de49bb896f5353bfce
Andressa Toledo <andressa.toledoss@gmail.com>
2025-09-25 21:05:27 -0300

    Feat(Menu): Inicio do menu lateral

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,12 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import MapView from '../pages/MapView';
 
-import Home from '../components/Home';
-
 export default function Rotas() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
       <Route path="/map" element={<MapView />} />
     </Routes>
   );
